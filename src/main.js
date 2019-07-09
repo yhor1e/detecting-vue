5d6e3cca56f47e9c92e48e62fa349f7aa436f867
yhorie <yuk.horie@gmail.com>
2019-07-09 21:31:36 +0900

    Polish

--- src/main.js
+++ src/main.js
@@ -8,25 +8,28 @@ const First = {
                  <li v-for="item in lists" :key="item.id">{{ item.val }}</li>
                </ul>
             </div>`,
-  data: function(){
+  data: function() {
     return {
       lists: []
     };
   },
-  created(){
-    this.lists = localStorage.getItem('lists') == null ? [] : JSON.parse(localStorage.getItem('lists'));
+  created() {
+    this.lists =
+      localStorage.getItem("lists") == null
+        ? []
+        : JSON.parse(localStorage.getItem("lists"));
   },
   methods: {
-    addItem(e){
+    addItem(e) {
       this.lists.push({
         val: e.target.value,
-        id: (new Date()).getTime()
+        id: new Date().getTime()
       });
-      localStorage.setItem('lists', JSON.stringify(this.lists));
+      localStorage.setItem("lists", JSON.stringify(this.lists));
     },
     reset() {
       this.lists = [];
-      localStorage.setItem('lists', JSON.stringify(this.lists));
+      localStorage.setItem("lists", JSON.stringify(this.lists));
     }
   }
 };
@@ -37,15 +40,15 @@ const Second = {
             </div>`
 };
 
-Vue.component('first', First);
-Vue.component('second', Second);
+Vue.component("first", First);
+Vue.component("second", Second);
 const app = new Vue({
   data: {
-    target: 'first'
+    target: "first"
   },
   methods: {
     onChange(target) {
       this.target = target;
     }
   }
-}).$mount('#app');
+}).$mount("#app");
